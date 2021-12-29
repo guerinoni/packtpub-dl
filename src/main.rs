acb347b0d725613110912d282067a6e1e8183d09
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-12-29 21:45:25 +0100

    Download the file requested
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@
 // # URL to get url file to download, params are book id and format of the file (can be pdf, epub, etc..), method GET
 // URL_BOOK_ENDPOINT = "products-v1/products/{book_id}/files/{format}"
 
+use futures_util::StreamExt;
+use std::io::Write;
+
 #[derive(serde::Deserialize)]
 struct UserInfo {
     data: Data,
@@ -25,13 +28,18 @@ struct Data {
 
 #[derive(serde::Deserialize)]
 struct Book {
-    data: Vec<BookInfo>
+    data: Vec<BookInfo>,
 }
 
 #[derive(serde::Deserialize)]
 struct BookInfo {
     #[serde(rename = "productId")]
     product_id: String,
+}
+
+#[derive(serde::Deserialize)]
+struct DownloadUrl {
+    data: String,
 }
 
 #[tokio::main]
@@ -57,9 +65,38 @@ async fn main() -> Result<(), reqwest::Error> {
 
     // TODO: make offset and limit configurable
     let url = "https://services.packtpub.com/entitlements-v1/users/me/products?sort=createdAt:DESC&offset=0&limit=1";
-    let req = client.get(url).headers(headers).send().await?;
+    let req = client.get(url).headers(headers.clone()).send().await?;
     let t = req.json::<Book>().await?;
-    // let product = t.data[0].product_id;
+
+    // TODO: make pdf configurable?
+    let url = format!(
+        "https://services.packtpub.com/products-v1/products/{}/files/pdf",
+        t.data[0].product_id
+    );
+    let res = client.get(url).headers(headers).send().await?;
+    let t = res.json::<DownloadUrl>().await?;
+
+    let response = client.get(t.data).send().await?;
+    let size = response
+        .content_length()
+        .ok_or("failed to get content size")
+        .unwrap();
+    let mut file = std::fs::File::create("book.pdf")
+        .or(Err("failed to create file"))
+        .unwrap();
+    let mut downloaded: u64 = 0;
+    let mut stream = response.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item
+            .or(Err(format!("Error while downloading file")))
+            .unwrap();
+        file.write(&chunk)
+            .or(Err(format!("Error while writing to file")))
+            .unwrap();
+        let new = std::cmp::min(downloaded + (chunk.len() as u64), size);
+        downloaded = new;
+    }
 
     Ok(())
 }
