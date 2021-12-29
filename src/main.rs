e25f417e8d106d3b1fa03ece2386a7d7deca0467
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-12-29 22:10:19 +0100

    Make use more consistent
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use futures_util::StreamExt;
+use reqwest::header::{HeaderMap, HeaderValue};
+use std::collections::HashMap;
 use std::io::Write;
 
 #[derive(serde::Deserialize)]
@@ -35,40 +37,69 @@ async fn main() {
         return;
     }
 
-    let email = args[1].as_str();
-    let password = args[2].as_str();
-
-    let url = "https://services.packtpub.com/auth-v1/users/tokens";
-    let mut map = std::collections::HashMap::new();
-    map.insert("username", email);
-    map.insert("password", password);
     let client = reqwest::Client::new();
-    let post = client.post(url).json(&map).send().await.map_err(|e|e.to_string()).unwrap();
-    let j = post.json::<UserInfo>().await.map_err(|e|e.to_string()).unwrap();
+    let url = "https://services.packtpub.com/auth-v1/users/tokens";
+    let hm = HashMap::from([
+        ("username", args[1].as_str()),
+        ("password", args[2].as_str()),
+    ]);
+    let post = client
+        .post(url)
+        .json(&hm)
+        .send()
+        .await
+        .map_err(|e| e.to_string())
+        .unwrap();
+
+    let j = post
+        .json::<UserInfo>()
+        .await
+        .map_err(|e| e.to_string())
+        .unwrap();
     let mut token = String::from("Bearer ");
     token.push_str(j.data.access.as_str());
 
-    let mut headers = reqwest::header::HeaderMap::new();
-    headers.insert("User-Agent", reqwest::header::HeaderValue::from_static("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"));
-    headers.insert(
-        "Authorization",
-        reqwest::header::HeaderValue::from_str(&token).unwrap(),
-    );
+    let mut headers = HeaderMap::new();
+    headers.insert("User-Agent", HeaderValue::from_static("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"));
+    headers.insert("Authorization", HeaderValue::from_str(&token).unwrap());
 
     // TODO: make offset and limit configurable
+    // TODO: take latest from the list and not only one...
+    // HACK: print the list and let user choiches
     let url = "https://services.packtpub.com/entitlements-v1/users/me/products?sort=createdAt:DESC&offset=0&limit=1";
-    let req = client.get(url).headers(headers.clone()).send().await.map_err(|e|e.to_string()).unwrap();
-    let t = req.json::<Book>().await.map_err(|e|e.to_string()).unwrap();
+    let req = client
+        .get(url)
+        .headers(headers.clone())
+        .send()
+        .await
+        .map_err(|e| e.to_string())
+        .unwrap();
+    let t = req.json::<Book>().await.map_err(|e| e.to_string()).unwrap();
 
     // TODO: make pdf configurable?
     let url = format!(
         "https://services.packtpub.com/products-v1/products/{}/files/pdf",
         t.data[0].product_id
     );
-    let res = client.get(url).headers(headers).send().await.map_err(|e|e.to_string()).unwrap();
-    let t = res.json::<DownloadUrl>().await.map_err(|e|e.to_string()).unwrap();
+    let res = client
+        .get(url)
+        .headers(headers)
+        .send()
+        .await
+        .map_err(|e| e.to_string())
+        .unwrap();
+    let t = res
+        .json::<DownloadUrl>()
+        .await
+        .map_err(|e| e.to_string())
+        .unwrap();
 
-    let response = client.get(t.data).send().await.map_err(|e|e.to_string()).unwrap();
+    let response = client
+        .get(t.data)
+        .send()
+        .await
+        .map_err(|e| e.to_string())
+        .unwrap();
     let size = response
         .content_length()
         .ok_or("failed to get content size")
